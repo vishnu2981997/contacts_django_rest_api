ec0cce2f2e02c1139377e9e1a0b5456c6d78fc55
vishnu2981997 <vishnu2981997@gmail.com>
2019-08-28 19:28:30 +0530

    added actions in views

--- contacts/views.py
+++ contacts/views.py
@@ -27,6 +27,37 @@ class UserViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save()
 
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all().order_by('id')
+        else:
+            return User.objects.none()
+
+    @action(detail=False)
+    def recent_users(self, request):
+        recent_users = User.objects.all().order_by('-last_login')
+
+        return self.order(recent_users)
+
+    @action(detail=False, permission_classes=[permissions.IsAdminUser])
+    def active_users(self, request):
+        """
+        Returns active users
+        """
+        active_users = User.objects.filter(is_active=True).order_by("id")
+
+        return self.order(active_users)
+
+    def order(self, data):
+        page = self.paginate_queryset(data)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(data, many=True)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
+
 
 class ContactViewSet(viewsets.ModelViewSet):
     """
@@ -51,3 +82,60 @@ class ContactViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Contact.objects.filter(owner=self.request.user)
+
+    # uncomment to modify retrieve operation
+
+    # def retrieve(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     if instance.owner == self.request.user:
+    #         serializer = self.get_serializer(instance)
+    #         headers = self.get_success_headers(serializer.data)
+    #         return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
+    #     else:
+    #         return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    @action(detail=False)
+    def order_by_name(self, request):
+        """
+        Returns contact data ordered by name
+        """
+        contacts = Contact.objects.filter(owner=self.request.user).order_by('name')
+
+        return self.order(contacts)
+
+    @action(detail=False)
+    def order_by_id(self, request):
+        """
+        Returns contact data ordered by id
+        """
+        contacts = Contact.objects.filter(owner=self.request.user).order_by('id')
+
+        return self.order(contacts)
+
+    @action(detail=False)
+    def order_by_email(self, request):
+        """
+        Returns contact data ordered by email
+        """
+        contacts = Contact.objects.filter(owner=self.request.user).order_by('email')
+
+        return self.order(contacts)
+
+    @action(detail=False)
+    def recently_created(self, request):
+        """
+        Returns recently created contacts
+        """
+        contacts = Contact.objects.filter(owner=self.request.user).order_by('-created')
+
+        return self.order(contacts)
+
+    def order(self, data):
+        page = self.paginate_queryset(data)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(data, many=True)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
