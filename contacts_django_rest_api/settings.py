7b80a199c0419612dada5a20c04dbf4ddf4c6ed8
vishnu2981997 <vishnu2981997@gmail.com>
2019-09-06 00:19:37 -0700

    added support for default sqlite3 in settings.py

--- contacts_django_rest_api/settings.py
+++ contacts_django_rest_api/settings.py
@@ -73,16 +73,32 @@ WSGI_APPLICATION = 'contacts_django_rest_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': config("ENGINE"),
-        'NAME': config("NAME"),
-        "USER": config("USER"),
-        "PASSWORD": config("PASSWORD"),
-        "HOST": config("HOST"),
-        "PORT": config("PORT")
+database = config("DATABASE")
+if database == "custom":
+    DATABASES = {
+        'default': {
+            'ENGINE': config("ENGINE"),
+            'NAME': config("NAME"),
+            "USER": config("USER"),
+            "PASSWORD": config("PASSWORD"),
+            "HOST": config("HOST"),
+            "PORT": config("PORT")
+        }
     }
-}
+elif database == "default":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
